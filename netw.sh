9f5eee43d7ddbb5c56822876405881b022476d26
Valery Girkin <vary10@gmail.com>
2016-01-16 21:42:46 +0300

    f

---
+++ netw.sh
@@ -0,0 +1,19 @@
+# !/bin/bash
+
+
+cat > /etc/network/interfaces << -EOF
+
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+#eth0
+auto eth0
+iface eth0 inet static
+address 192.168.0.22
+net mask 255.255.255.0
+gateway 192.168.0.1
+
